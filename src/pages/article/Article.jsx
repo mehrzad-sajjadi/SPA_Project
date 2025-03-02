2fa9896a0e8eedc1882317db0cfcd632a745131c
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-03-03 01:54:36 +0330

    commit

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -1,4 +1,4 @@
-import { Routes,Route,Link,Outlet  } from 'react-router-dom';
+import { Routes,Route,Link ,Outlet } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import Header from "../../components/Header";
 function Article(){
@@ -20,7 +20,15 @@ function Article(){
             <h1 className="py-4 text-center ">صفحه مقالات</h1>
             <hr />
             <div className='flex justify-center '>
-                
+                <Button className='mx-2' >
+                    <Link to='/article/php' className="text-white no-underline">php</Link>
+                </Button>
+                <Button className='mx-2'>
+                    <Link to='/article/js' className="text-white no-underline">JavaScript</Link>
+                </Button>
+                <Button className='mx-2'>
+                    <Link to='/article/react' relative='path' className="text-white no-underline">React</Link>
+                </Button>
             </div>
 
             {/* <div className='flex justify-center py-5'>
@@ -29,7 +37,11 @@ function Article(){
                 <Link to='/article/react'>   React       </Link>
             </div> */}
             <hr />
-            
+            <Routes>
+                <Route path='php' element={<h2 className='text-center'>php article</h2>} />
+                <Route path='js' element={<h2 className='text-center'>Javascript article</h2>} />
+                <Route path='react' element={<h2 className='text-center'>React article</h2>} />
+            </Routes>        
         </>
     )
 }
