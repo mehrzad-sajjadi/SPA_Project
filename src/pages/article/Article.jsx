a96d5e4d79dc7f948ee3fb5572d6fb57fc201246
amirhosein sajjadi <canadavancouver2003@gmail.com>
2025-03-03 01:41:36 +0330

    commit

--- src/pages/article/Article.jsx
+++ src/pages/article/Article.jsx
@@ -19,24 +19,24 @@ function Article(){
             <Header></Header>
             <h1 className="py-4 text-center ">صفحه مقالات</h1>
             <hr />
-            {/* <div className='flex justify-center py-5'>
-                <Button className='mx-2' >
-                    <Link to={routes.php.path} className="text-white no-underline">php</Link>
-                </Button>
-                <Button className='mx-2'>
-                    <Link to={routes.js.path} className="text-white no-underline">JavaScript</Link>
-                </Button>
-                <Button className='mx-2'>
-                    <Link to={routes.react.path}  relative='path' className="text-white no-underline">React</Link>
-                </Button>
-            </div> */}
-
             <div className='flex justify-center py-5'>
-                <Link to='php' >    php         </Link>
-                <Link to='js'  >    JavaScript  </Link>
-                <Link to='react'>   React       </Link>
+                <Button className='mx-2' >
+                    <Link to='/article/php' className="text-white no-underline">php</Link>
+                </Button>
+                <Button className='mx-2'>
+                    <Link to='/article/js' className="text-white no-underline">JavaScript</Link>
+                </Button>
+                <Button className='mx-2'>
+                    <Link to='/article/react' relative='path' className="text-white no-underline">React</Link>
+                </Button>
             </div>
 
+            {/* <div className='flex justify-center py-5'>
+                <Link to='/article/php' >    php         </Link>
+                <Link to='/article/js'  >    JavaScript  </Link>
+                <Link to='/article/react'>   React       </Link>
+            </div> */}
+            <hr />
             <Routes>
                 <Route path='php' element={<h2 className='text-center'>php article</h2>} />
                 <Route path='js' element={<h2 className='text-center'>Javascript article</h2>} />
